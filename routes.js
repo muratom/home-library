075f7e229b13528ab16946b99ddf63f6041121bb
muratom <rm070201wr@gmail.com>
2021-10-01 23:30:56 +0300

    Add dummy pages of the book

--- routes.js
+++ routes.js
@@ -1,24 +1,38 @@
 const router = require("express").Router();
 
+const userName = "Ruslan";
+
 // Get the books data from the server
-let libBooks = require("./data/books.json")
+let libBooks = require("./data/books.json");
 
 // Main page
 router.get("/", (req, res) => {
   res.render("main", {
-    user_name: "Ruslan",
+    user_name: userName,
     books: libBooks.books
-  })
-})
+  });
+});
+
+router.get("/books/:id", (req, res) => {
+  res.render("book", {
+    user_name: userName,
+    book: libBooks.books[req.params.id - 1]
+  });
+});
 
 // Access to the style
 router.get("/styles/:css_file_name", (req, res) => {
   res.sendFile(__dirname + "/styles/" + req.params.css_file_name);
-})
+});
 
 // Access to the image
 router.get("/images/:image", (req, res) => {
   res.sendFile(__dirname + "/images/" + req.params.image);
-})
+});
 
-module.exports = router
+module.exports = router;
+
+/*
+TODO:
+  Error handling
+ */
