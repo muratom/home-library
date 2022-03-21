1ddade81bfbf2747fc8eb58465dc7fba38b5f317
muratom <rm070201wr@gmail.com>
2022-03-21 16:37:41 +0300

    Fix regex bug in a book addition

--- routes.js
+++ routes.js
@@ -50,7 +50,7 @@ router.post("/books/adding", (req, res) => {
     "whenMustReturn": null
   };
   if (!newBook.title ||
-    !newBook.author.toString().match(/[A-Za-z]+/) ||
+    !newBook.author ||
     !newBook.published.toString().match(/^[0-9]+$/)) {
     res.status(400);
     res.json({message: "Bad request"});
